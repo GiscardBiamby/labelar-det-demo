a64d472c0789d6709cdde9639eef7ceedf5e06e6
Giscard Biamby <giscard@gmail.com>
2019-10-09 15:44:53 -0700

    Got training running with bidmugs data under tensorflow

---
+++ training/tfmodels/research/train_bidmugs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# From the tensorflow/models/research/ directory
+PIPELINE_CONFIG_PATH=./object_detection/samples/configs/ssd_mobilenet_v2_bidmugs.config
+MODEL_DIR=../../weights/ssd_mobilenet_v2_bidmugs
+NUM_TRAIN_STEPS=50000
+SAMPLE_1_OF_N_EVAL_EXAMPLES=1
+python object_detection/model_main.py \
+    --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
+    --model_dir=${MODEL_DIR} \
+    --num_train_steps=${NUM_TRAIN_STEPS} \
+    --sample_1_of_n_eval_examples=$SAMPLE_1_OF_N_EVAL_EXAMPLES \
+    --alsologtostderr
